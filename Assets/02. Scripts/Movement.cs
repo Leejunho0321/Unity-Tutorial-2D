95dab1cd9d3aca6f2dcec96f9a86db1227ea5751
Leejunho0321 <okayyo0321@gmail.com>
2025-05-20 10:57:54 +0900

    [ADD] Unity Project 2025-05-20
    냠냠
    쩝쩝
    꿀꺽

---
+++ Assets/02. Scripts/Movement.cs
@@ -0,0 +1,26 @@
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/02. Scripts/StudyLog.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class StudyLog : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Start 함수");
+        Debug.LogWarning("Start 함수");
+        Debug.LogError("Start 함수");
+    }
+
+    private void Update()
+    {
+        
+    }
+}
